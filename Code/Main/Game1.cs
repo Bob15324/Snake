39c73c39541349a6fe63614c2ae4f5ee4fcc1be2
Bob15324 <57432858+Bob15324@users.noreply.github.com>
2023-03-04 23:42:39 -0800

    Initial commit

---
+++ Code/Main/Game1.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Design;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Snake;
+
+public class Game1 : Game
+{
+    public static GraphicsDeviceManager _graphics;
+    public static SpriteBatch _spriteBatch;
+    public static int Width = 1000;
+    public static int height = 800; 
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+
+        base.Initialize();
+    }
+
+    protected override void UnloadContent()
+    {
+        base.UnloadContent();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.Black);
+
+        base.Draw(gameTime);
+    }
+}
---
+++ Code/Snake/Snakes.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Design;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Snake
+{
+    public class Snakes
+    {
+        Texture2D _texture = new Texture2D(graphicsDevice, Game1.Width / 9, Game1.height / 9);
+    }
+}
